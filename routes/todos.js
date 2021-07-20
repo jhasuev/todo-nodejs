f1bc57706a2201fcc4f5d118153a355627e06a0e
Jamal Hasuev <metafra@mail.ru>
2021-07-21 02:29:08 +0300

    add pagination logic

--- routes/todos.js
+++ routes/todos.js
@@ -1,4 +1,21 @@
 const Todo = require("../schemes/todo")
+const getTimeFrom = hm => {
+  const date = new Date(hm)
+  const times = [
+    date.getFullYear(),
+    '.',
+    date.getMonth() + 1,
+    '.',
+    date.getDate(),
+    ' ',
+    date.getHours(),
+    ':',
+    date.getMinutes(),
+    ':',
+    date.getSeconds(),
+  ]
+  return times.join('')
+}
 
 module.exports = app => {
   const pageDesctiption = {
@@ -6,29 +23,25 @@ module.exports = app => {
     heading: "Все задачи",
   }
 
-  app.get("/todos", async (req, res) => {
+  app.get(["/todos", "/todos/page", "/todos/page/:page"], async (req, res) => {
     if (!req.session.user) return res.redirect("/login")
-    
-    let todos = await Todo.find({ uid: req.session.user }).sort({ created: -1 })
-    todos = JSON.parse(JSON.stringify(todos))
-    todos.forEach(todo => {
-      const date = new Date(todo.created)
-      const times = [
-        date.getFullYear(),
-        '.',
-        date.getMonth() + 1,
-        '.',
-        date.getDate(),
-        ' ',
-        date.getHours(),
-        ':',
-        date.getMinutes(),
-        ':',
-        date.getSeconds(),
-      ]
-      todo.created = times.join('')
-    })
-    res.render("todos", { ...pageDesctiption, todos })
+    const WHERE = { uid: req.session.user }
+    const page = Math.abs(Number(req.params.page) || 1)
+    const count = await Todo.count( WHERE )
+    const max = 5
+    const skip = max * (page - 1)
+    const pages = Math.ceil(count / max)
+    const pagination = { page, pages }
+
+    let todos = await Todo.find( WHERE ).sort({ created: -1 }).skip( skip ).limit( max )
+    if (todos && todos.length) {
+      todos = JSON.parse(JSON.stringify(todos))
+      todos.forEach(todo => todo.created = getTimeFrom(todo.created))
+  
+      res.render("todos", { ...pageDesctiption, todos, pagination })
+    } else {
+      res.render("todos", { ...pageDesctiption, heading: "нет записей..." })
+    }
   })
 
   app.get("/todos/remove/:id", async (req, res) => {
